c9a91b468c576da8da1684b55027562b300fe522
Jesus Hernandez <jesushdzusa@gmail.com>
2025-04-23 01:26:37 -0500

    1.11*: unicafe step 6

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import './App.css';
 
 const Button = (props) => {
   return (
@@ -6,14 +7,18 @@ const Button = (props) => {
   )
 
 }
-const StatisticLine=(props)=>{
-  return(
-    <p>{props.text} {props.value}</p>
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+
   )
 }
 
 const Statistics = (props) => {
-  const {good,neutral,bad} = props.sentiment;
+  const { good, neutral, bad } = props.sentiment;
   const calculateAverage = () => {
     return props.total === 0 ? 0 : props.score / props.total;
   }
@@ -23,24 +28,32 @@ const Statistics = (props) => {
   }
 
   if (props.total === 0) {
-    return(
+    return (
       <h3>No feedback given</h3>
     )
   } else {
     return (
-      <div>
-        <h2>statistics</h2>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="total" value={props.total} />
-        <StatisticLine text="average" value={calculateAverage()} />
-        <StatisticLine text="positive" value={`${calculatePositives()*100} %`} /> 
-       
-      </div>
-    )  
+      <table>
+        <caption>Statistics</caption>
+        <thead>
+          <tr>
+            <th>Sentiment</th>
+            <th>Stat</th>
+          </tr>
+        </thead>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="total" value={props.total} />
+          <StatisticLine text="average" value={calculateAverage().toFixed(2)} />
+          <StatisticLine text="positive" value={`${(calculatePositives() * 100).toFixed(2)} %`} />
+          </tbody>
+        
+      </table>
+    )
   }
-  
+
 
 }
 
@@ -69,17 +82,21 @@ const App = () => {
     setScore(score - 1);
   }
 
-  
+
 
   return (
+    <>
     <div>
-      <h1>give feedback</h1>
+      <h1>Give Feedback</h1>
       <Button onClick={handleGoodClick} text="good" />
       <Button onClick={handleNeutralClick} text="neutral" />
       <Button onClick={handleBadClick} text="bad" />
-      <Statistics sentiment={{good,neutral,bad}} total={allfeed} score ={score} />
     </div>
+    <br></br>
+    <Statistics sentiment={{ good, neutral, bad }} total={allfeed} score={score} />
 
+    </>
+    
   )
 
 }
