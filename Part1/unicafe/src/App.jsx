4b03a05d5f0b7085a47122afb194fd49365cc369
Jesus Hernandez <jesushdzusa@gmail.com>
2025-04-23 00:12:15 -0500

    1.8: unicafe step 3

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,44 +1,61 @@
 import { useState } from "react";
 
-const Button=(props)=>{
-  return(
+const Button = (props) => {
+  return (
     <button onClick={props.onClick} >{props.text}</button>
   )
 
 }
 
-const App=()=>{
+const Statistics = (props) => {
+  const {good,neutral,bad} = props.sentiment;
+  const calculateAverage = () => {
+    return props.total === 0 ? 0 : props.score / props.total;
+  }
+
+  const calculatePositives = () => {
+    return props.total === 0 ? 0 : good / props.total;
+  }
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {props.total}</p>
+      <p>average {calculateAverage()}</p>
+      <p>positive {calculatePositives() * 100} %</p>
+    </div>
+  )
+
+}
+
+const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [allfeed, setAllfeed] = useState(0);
   const [score, setScore] = useState(0);
 
-  const handleGoodClick=()=>{    
-    setGood(good+1);
-    setAllfeed(allfeed+1);
-    setScore(score+1);
-  }
-  
-  const handleNeutralClick=()=>{
-    setNeutral(neutral+1);
-    setAllfeed(allfeed+1);
-    
-  }
-  
-  const handleBadClick=()=>{
-    setBad(bad+1);
-    setAllfeed(allfeed+1);
-    setScore(score-1);
-  }
-  
-  const calculateAverage=()=>{
-    return allfeed === 0? 0:score/allfeed; 
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    setAllfeed(allfeed + 1);
+    setScore(score + 1);
   }
 
-  const calculatePositives =()=>{
-    return allfeed === 0? 0:good/allfeed;  
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    setAllfeed(allfeed + 1);
+
   }
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+    setAllfeed(allfeed + 1);
+    setScore(score - 1);
+  }
+
+  
 
   return (
     <div>
@@ -46,14 +63,9 @@ const App=()=>{
       <Button onClick={handleGoodClick} text="good" />
       <Button onClick={handleNeutralClick} text="neutral" />
       <Button onClick={handleBadClick} text="bad" />
-      <h2>statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {allfeed}</p>
-      <p>average {calculateAverage()}</p>
-      <p>positive {calculatePositives()*100} %</p>
+      <Statistics sentiment={{good,neutral,bad}} total={allfeed} score ={score} />
     </div>
+
   )
 
 }
