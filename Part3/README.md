701931eba0e4d57df6ebf1f1da318ae52aaac39e
Jesus Hernandez <jesushdzusa@gmail.com>
2025-05-09 13:44:18 -0500

    removing files from github

---
+++ Part3/README.md
@@ -0,0 +1,9 @@
+1.- This folder contains a copy of the frontend application made on part2 (phonebook) renamed as phonebookfrontend, over this copy I made all the changes needed to test the fullapplication and create a "dist" file to add in the phonebookbackend.
+
+2.- To deploy the backend and then the full application I had to create a new repository called "phonebookbackend" I pushed all the files located in FSHelsinki/Part3/phonebookbackend to this new repository.
+
+3.- This is the application address on Render https://phonebookbackend-e20o.onrender.com/
+
+4.- Commits for exercises 3.1 to 3.9 are found on https://github.com/StarkNeo/FSHelsinki/Part3/phonebookbackend
+
+5.- Commit for exercises 3.10 to 3.11 are found on https://github.com/StarkNeo/phonebookbackend
--- notesBackend/index.js
+++ notesBackend/index.js
@@ -1,10 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 const app=express();
+const dotenv = require('dotenv');
+dotenv.config();
+const Note = require('./models/note')
+const { default: mongoose } = require('mongoose');
+
 app.use(express.json())
 app.use(cors());
 app.use(express.static('build'))
+/*
+let USER = process.env.USER;
+let PASS = process.env.PASS;
 
+let url = `mongodb+srv://${USER}:${PASS}@cluster0.niabjhj.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false);
+mongoose.connect(url);
+
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean
+})
+
+const Note = mongoose.model('Note',noteSchema);
+/*We also don't want to return the mongo versioning field __v to the frontend.
+
+One way to format the objects returned by Mongoose is to modify the toJSON method of the schema, which is used on all instances of the models produced with that schema.
+*/
+/*noteSchema.set('toJSON',{
+    transform:(document, returnedObject)=>{
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+*/
 
 let notes = [
     {
@@ -35,18 +67,23 @@ app.get('/',(request, response)=>{
 })
 
 app.get('/api/notes',(request, response)=>{
-    response.json(notes)
+    Note.find({}).then(notes=>{
+        response.json(notes)
+    }) 
 })
 
 /*The unique address we will use for an individual note is of the form notes/10, where the number at the end refers to the note's unique id number.
 We can define parameters for routes in Express by using the colon syntax:*/
 
 app.get('/api/notes/:id',(request, response)=>{
-    const id =  request.params.id;
+    /*const id =  request.params.id;
     const note = notes.find(note=>note.id===id)
     // If no note is found, the server should respond with the status code 404 not found instead of 200.//
     if(note) response.json(note)
-        else response.status(404).end()
+        else response.status(404).end()*/
+    Note.findById(request.params.id).then(note=>{
+        response.json(note)
+    })
 })
 
 app.delete('/api/notes/:id',(request,response)=>{
@@ -61,17 +98,25 @@ app.post('/api/notes',(request, response)=>{
         return response.status(400).json({error:'content missing'})
     }
 
-    let note={
+    let note = new Note({
         content:body.content,
         important:body.important||false,
-        id:generateId(),
-    }
-    notes = notes.concat(note)
-    response.json(note)
+        //id:generateId(),
+    });
+    //notes = notes.concat(note)
+    note.save().then(savedNote=>{
+        response.json(savedNote)
+    })
+    //response.json(note)
 })
 
 app.put('/api/notes/:id',(request, response)=>{
     let id = request.params.id;
+    let note = new Note(request.body)
+    note.save().then(updatedNote=>{
+        response.json(updatedNote)
+    })
+    /*
     let oldNote = notes.find(note=>note.id===id);
     if(oldNote){
         let updatedNote = request.body
@@ -83,11 +128,12 @@ app.put('/api/notes/:id',(request, response)=>{
         console.log(notes)        
         return response.send(updatedNote)
     }
-    response.status(404).end()
+    response.status(404).end()*/
     
 })
 
-const PORT = process.env.PORT||3001
+
+const PORT = process.env.PORT
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
 })
