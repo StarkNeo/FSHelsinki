240177a34aa59e34b39e6cda37804bbebce3c5ca
Jesus Hernandez <jesushdzusa@gmail.com>
2025-05-09 16:21:44 -0500

    updating mongoose

--- Part3/phonebookbackend/index.js
+++ Part3/phonebookbackend/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
-const dotenv = require('dotenv');
-dotenv.config()
+//const dotenv = require('dotenv');
+//dotenv.config()
 const Contact = require('./models/phonebook');
 
 
@@ -11,7 +11,6 @@ app.use(cors());
 app.use(express.static('dist'));
 app.use(express.json());
 //app.use(morgan('tiny'));
-
 
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
@@ -26,7 +25,7 @@ let infoPage = (contacts) => {
 
 }
 
-let PORT = process.env.PORT;
+let PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
