dfafad32f2e475c788d5c0510eb7d78ade621f14
Jesus Hernandez <jesushdzusa@gmail.com>
2025-05-19 17:43:52 -0500

    4.5*: Helper Functions and Unit Tests, step 3

--- Part4/bloglistbackend/tests/helpers.test.js
+++ Part4/bloglistbackend/tests/helpers.test.js
@@ -9,6 +9,7 @@ test('dummy returns one', () => {
   assert.strictEqual(result, 1)
 })
 
+
 describe('total likes', () => {
 
 
@@ -51,5 +52,36 @@ describe('total likes', () => {
     assert.strictEqual(result, 12)
   })
 
+
+})
+
+describe('maximum likes, return the object with maximum likes among the others', () => {
+  test('maximum likes return object with 7 likes', () => {
+    let blogs = [{
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },]
+    const result = listHelper.favoriteBlog(blogs)
+    assert.deepStrictEqual(result, {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    })
+  })
 
 })
--- Part4/bloglistbackend/utils/list_helper.js
+++ Part4/bloglistbackend/utils/list_helper.js
@@ -6,8 +6,18 @@ const sumLikes=(blogs)=>{
   return blogs.reduce((sum,item)=>sum+item.likes,0)
 }
 
+const favoriteBlog=(blogs)=>{
+  let maximum = blogs.reduce((maxBlog, blog)=>{
+    return blog.likes > maxBlog.likes ? blog: maxBlog,blogs[0]
+  })
+  console.log(maximum)
+  return maximum
+
+}
+
 module.exports = {
   dummy,
-  sumLikes
+  sumLikes,
+  favoriteBlog
 }
 
