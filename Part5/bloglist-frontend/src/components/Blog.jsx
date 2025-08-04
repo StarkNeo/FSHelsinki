f40f324c452c628ccd9b0298cb5c7ef80281c950
Jesus Hernandez <jesushdzusa@gmail.com>
2025-08-04 09:41:49 -0500

    5.7 Blog List Frontend, step 7

--- Part5/bloglist-frontend/src/components/Blog.jsx
+++ Part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
+import { useState } from "react"
+
+
+const Blog = ({ blog }) => {
+  const [viewDetails, setViewDetails]=useState(false)
+
+  const handleViewDetails=()=>{
+    setViewDetails(!viewDetails)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+
+  return (
+  <div style={blogStyle}>
+    <div>
+      {blog.title} {blog.author}
+      <button onClick={handleViewDetails}>{viewDetails?'hide':'view'}</button>
+    </div>
+    {viewDetails?<div>
+      {blog.url}
+    <div>
+      Likes 0
+      <button>Like</button>
+      </div>  
+    </div>:'' }
+    
+  </div>
+
 )
+    
+}
 
 export default Blog
