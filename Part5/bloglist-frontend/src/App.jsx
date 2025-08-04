813bc8b61a11f5ec6488383dab40d4a219247e62
Jesus Hernandez <jesushdzusa@gmail.com>
2025-08-04 12:30:44 -0500

    5.10: Blog List Frontend, step 10

--- Part5/bloglist-frontend/src/App.jsx
+++ Part5/bloglist-frontend/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   
   useEffect(() => {
     blogService.getAll()
-      .then(response => setBlogs(response))
+      .then(response => setBlogs(response.sort((a,b)=>a.likes-b.likes)))
 
   }, [])
 
